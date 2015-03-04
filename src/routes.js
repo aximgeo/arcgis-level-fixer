2afd5b044df2e1b026ceb34561ef678c78ed0e6e
Chris Bupp <cbupp@gisinc.com>
2015-03-04 13:25:12 -0500

    Last bit of tweaks before GitHub update

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,26 @@
 var ArcGISController = require('./ArcGISController.js').ArcGISController,
-    arcGISController = new ArcGISController();
+    arcGISController = new ArcGISController(),
+    path = require('path'),
+    appDir = path.dirname(require.main.filename);
 
 exports.setup = function (app) {
     "use strict";
     app.get('[/]?', function(req, res) {
-        res.sendfile('index.html');
+      var options = {
+        root: appDir,
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+      };
+
+      res.sendFile("index.html", options, function (err) {
+        if (err) {
+          console.log(err);
+          res.status(err.status).end();
+        }
+      });
     });
     app.get('/examine', arcGISController.getRedirectUrl.bind(arcGISController));
     app.get('*', arcGISController.performRedirectUrl.bind(arcGISController));
